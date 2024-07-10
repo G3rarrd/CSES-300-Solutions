427a629b62381634b3e73224d57f2dda70945f4f
G3rrard <gnwokeduko@gmail.com>
2024-07-10 15:00:31 +0100

    First Solution to Missing Number

---
+++ Introductory_Problems/Missing_Number.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main () {
+    int n;
+    cin >> n;
+    int c = 0;
+    
+    for (int i = 0; i < n -1; i++) {
+        int x;
+        cin >> x;
+        c ^= x;
+    }
+ 
+    for (int i = 1; i <= n; i++) {
+        c ^= i;
+    }
+ 
+    cout << c;
+}
