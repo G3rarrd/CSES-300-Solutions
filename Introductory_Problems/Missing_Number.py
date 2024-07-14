d47b22564e15af0b649071a341b9b1d8a61e2e66
G3rrard <gnwokeduko@gmail.com>
2024-07-14 11:48:05 +0100

    Python Solution to Missing Number

---
+++ Introductory_Problems/Missing_Number.py
@@ -0,0 +1,21 @@
+# Math method
+# 1. Use Gaussian summation to calculate the sum of numbers from 1 to n
+
+# 2. Get the total of the inputted values
+
+# 3. Subtract the total of the inputted values from the gaussian sum to get
+# the missing number
+
+def missingNumber(n):
+    ans = n * (n + 1) // 2 # Gaussian Summation
+    total = sum(list(map(int, input().split())))
+    return ans - total
+
+if __name__ == "__main__":
+    n = int(input())
+    print(missingNumber(n))
+
+
+
+        
+    
