07d550a9102e620e5da1cc3a26e74a090af39957
G3rrard <gnwokeduko@gmail.com>
2024-07-14 12:08:16 +0100

    Python solution to Repetitions

---
+++ Introductory_Problems/Repetitions.py
@@ -0,0 +1,18 @@
+def repetitions(s):
+    n = len(s)
+    i = 0
+    maxRep = 1
+    while(i < n):
+        j = i + 1
+        while (j < n and s[i] == s[j] ) :
+            j += 1
+        
+        maxRep = max(maxRep, (j - i) )
+        i = j
+        
+    return maxRep
+
+
+if __name__ == "__main__" :
+    s = input()
+    print(repetitions(s))
