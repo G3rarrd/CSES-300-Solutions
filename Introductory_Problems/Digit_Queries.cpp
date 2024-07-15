21b157c0de98fe05db3d698af9dbdfe90e8dfa2c
G3rrard <gnwokeduko@gmail.com>
2024-07-15 16:25:58 +0100

    C++ Solution to Digit_Queries

---
+++ Introductory_Problems/Digit_Queries.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int digitQueries(long long k) {
+    if (k < 10) {
+        return k;
+    }
+
+    // Initializing the start and end intervals
+    long long startInterval = 0;
+    long long endInterval = 9;
+
+    long long pow10 = 10;
+
+    long long digits = 1;
+
+    while (true) {
+        startInterval = endInterval + 1; 
+        digits++;
+        endInterval += 9*pow10*digits;
+
+        if (k > endInterval) break;
+
+        pow10 *= 10;
+    }
+
+    long long number = ((k - startInterval) / digits) + pow10;
+    long long index = (k - startInterval) % digits;
+
+    string numToString = to_string(number);
+
+    int res =  numToString[index] - '0';
+    return  res; 
+}
+
+int main() {
+    int q;
+    cin >> q;
+
+    for (int i = 0; i < q; i++) {
+        long long k;
+        cin >> k;
+        cout << digitQueries(k) << endl;
+    }
+    return 0;
+}
