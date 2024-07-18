bb8505d82515954e83efc01b93211495e0634270
G3rrard <gnwokeduko@gmail.com>
2024-07-18 20:35:38 +0100

    C++ Solution to Movie Festival

---
+++ Sorting_and_Searching/Movie_Festival.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct comp {
+    bool operator()(const pair<int, int>& a,const pair<int, int>& b) const {
+        return a.second < b.second;
+    }
+};
+
+int movieFestival(vector<pair<int, int>> timeFrames, int n) {
+    pair<int , int> curMovie = timeFrames[0];
+    int res = 1;
+    int i = 1;
+    
+    while (i < n) {
+        if (timeFrames[i].first >= curMovie.second) {
+            curMovie = timeFrames[i];
+            res++;
+        }
+        i++;
+    }   
+    return res;
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+    vector<pair<int, int>> timeFrames;
+
+    for (int i = 0; i < n; i++) {
+        int a;
+        int b;
+        cin >> a, cin >> b;
+        timeFrames.emplace_back(make_pair(a, b));
+    }
+
+    sort(timeFrames.begin(), timeFrames.end(), comp());
+    cout << movieFestival(timeFrames, n);
+    return 0;
+}
